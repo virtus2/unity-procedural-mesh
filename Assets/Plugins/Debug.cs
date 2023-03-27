d7f2242349a384bc220d66f797f52fd5de16a204
virtus2 <khd1323@naver.com>
2023-03-27 22:52:05 +0900

    Add Debug log stack trace function

--- Assets/Plugins/Debug.cs
+++ Assets/Plugins/Debug.cs
@@ -5,6 +5,9 @@
 // 
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using UnityEditor;
 using UnityEngine;
 
 public static class Debug 
@@ -43,5 +46,51 @@ public static class Debug
     public static void LogError(object message, Object context)
     {
         UnityEngine.Debug.LogError(message, context);
+    }
+
+    [UnityEditor.Callbacks.OnOpenAsset()]
+    private static bool OnOpenDebugLog(int instance, int line)
+    {
+        string name = EditorUtility.InstanceIDToObject(instance).name;
+        if (!name.Equals("Debug")) return false;
+
+        // 에디터 콘솔 윈도우의 인스턴스를 찾는다.
+        var assembly = Assembly.GetAssembly(typeof(EditorWindow));
+        if (assembly == null) return false;
+
+        var consoleWindowType = assembly.GetType("UnityEditor.ConsoleWindow");
+        if (consoleWindowType == null) return false;
+
+        var consoleWindowField = consoleWindowType.GetField("ms_ConsoleWindow", BindingFlags.Static | BindingFlags.NonPublic);
+        if (consoleWindowField == null) return false;
+
+        var consoleWindowInstance = consoleWindowField.GetValue(null);
+        if (consoleWindowInstance == null) return false;
+
+        if (consoleWindowInstance != (object)EditorWindow.focusedWindow) return false;
+
+        // 콘솔 윈도우 인스턴스의 활성화된 텍스트를 찾는다.
+        var activeTextField = consoleWindowType.GetField("m_ActiveText", BindingFlags.Instance | BindingFlags.NonPublic);
+        if (activeTextField == null) return false;
+
+        string activeTextValue = activeTextField.GetValue(consoleWindowInstance).ToString();
+        if (string.IsNullOrEmpty(activeTextValue)) return false;
+
+        // 디버그 로그를 호출한 파일 경로를 찾아 편집기로 연다.
+        Match match = Regex.Match(activeTextValue, @"\(at (.+)\)");
+        if (match.Success) match = match.NextMatch(); // stack trace의 첫번째를 건너뛴다.
+
+        if (match.Success)
+        {
+            string path = match.Groups[1].Value;
+            var split = path.Split(':');
+            string filePath = split[0];
+            int lineNum = System.Convert.ToInt32(split[1]);
+
+            string dataPath = UnityEngine.Application.dataPath.Substring(0, UnityEngine.Application.dataPath.LastIndexOf("Assets"));
+            UnityEditorInternal.InternalEditorUtility.OpenFileAtLineExternal(dataPath + filePath, lineNum);
+            return true;
+        }
+        return false;
     }
 }
