ce33a8de042dda2f31992cef9db03be6f9713216
virtus2 <khd1323@naver.com>
2023-04-07 23:23:23 +0900

    Add ReadOnly property

---
+++ Assets/Scripts/Utility/Editor/ReadOnlyDrawer.cs
@@ -0,0 +1,29 @@
+// 
+// Copyright (c) 2023 Kim Hyun Deok
+//
+// ReadOnlyDrawer.cs
+//
+using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+[CanEditMultipleObjects]
+public class ReadOnlyDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        EditorGUI.BeginProperty(position, label, property);
+
+        bool enabledState = GUI.enabled;
+        GUI.enabled = false;
+        EditorGUI.PropertyField(position, property, label, true);
+        GUI.enabled = enabledState;
+
+        EditorGUI.EndProperty();
+    }
+
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        return EditorGUI.GetPropertyHeight(property);
+    }
+}
