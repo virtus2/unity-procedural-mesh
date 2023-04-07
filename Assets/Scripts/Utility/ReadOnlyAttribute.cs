a666c1b97c1e9a289c7753e736f0c3f1abd61a09
virtus2 <khd1323@naver.com>
2023-04-07 23:22:14 +0900

    Add ReadOnly property

---
+++ Assets/Scripts/Utility/ReadOnlyAttribute.cs
@@ -0,0 +1,13 @@
+// 
+// Copyright (c) 2023 Kim Hyun Deok
+//
+// ReadOnlyAttribute.cs
+//
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadOnlyAttribute : PropertyAttribute
+{
+    // See ReadOnlyDrawer.cs
+}
